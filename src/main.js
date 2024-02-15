09d7b316ba90e41b166844ea8c9fe80200c80d62
ElyCarmona <elycarmona1997@gmail.com>
2024-02-15 15:57:22 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const config = {
+    title: 'Snake',
+    width: 320,
+    height: 180,
+    type: Phaser.AUTO,
+    parent: 'contenedor',
+    backgroundColor: '#f9ca24',
+    pixelArt: true
+};
+
+new Phaser.Game(config);
